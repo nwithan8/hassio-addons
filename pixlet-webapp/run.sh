0491a544491937e99f634408ff66ec241ff71b48
nwithan8 <nwithan8@users.noreply.github.com>
2023-12-28 15:16:03 -0700

    - Add Pixel Webapp

---
+++ pixlet-webapp/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Starting Pixlet Push..."
+
+# Prepare file system
+echo "Preparing file system..."
+mkdir -p /config/files || true
+
+# Add shortcuts to .bashrc so if/when user enters container, they're loaded
+echo "#!/bin/bash" >> /root/.bashrc
+echo "" >> /root/.bashrc
+echo "export PATH=$PATH:/pixlet" >> /root/.bashrc
+echo "export PIXLET_FILES_DIR=/config/files" >> /root/.bashrc
+
+# Start flask server
+/pixlet/pixlet serve /config/files/hello_world.star -i 0.0.0.0
