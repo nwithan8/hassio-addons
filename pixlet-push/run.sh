879c4b41fe4c2bdbcd3f922c3a9f9f38d757c92b
nwithan8 <nwithan8@users.noreply.github.com>
2023-12-28 15:07:30 -0700

    - Improve Pixlet Push mappings

--- pixlet-push/run.sh
+++ pixlet-push/run.sh
@@ -1,6 +1,16 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 
 echo "Starting Pixlet Push..."
+
+# Prepare file system
+echo "Preparing file system..."
+mkdir -p /config/files || true
+
+# Add shortcuts to .bashrc so if/when user enters container, they're loaded
+echo "#!/bin/bash" >> /root/.bashrc
+echo "" >> /root/.bashrc
+echo "export PATH=$PATH:/pixlet" >> /root/.bashrc
+echo "export PIXLET_FILES_DIR=/config/files" >> /root/.bashrc
 
 # Start flask server
 python3 /server/server.py
